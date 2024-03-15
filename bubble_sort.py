14a2f72e7dbc9b63f8bb7763ee6abbeaa3e47f20
carsonjf8 <carsonjf8@gmail.com>
2024-03-15 17:38:24 -0400

    fixed bug where last value was not sorted

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@
 def bubble_sort(arr: list) -> list:
     for i in range(len(arr)):
         swapped = False
-        for j in range(len(arr) - i  - 2):
+        for j in range(len(arr) - i  - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
